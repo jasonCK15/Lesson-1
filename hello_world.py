aea40b1d1dbfe32e22db700bcadcc2d5108d075a
jasonCK15 <jasoncheng1510@gmail.com>
2021-06-24 19:32:29 +0800

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print('Hello World')
+
+
+# # Heading
+# 
+# descriptive texts
+# 
+
+# In[6]:
+
+
+get_ipython().run_line_magic('timeit', '1+1')
+
+
+# In[7]:
+
+
+ls
+
+
+# In[9]:
+
+
+pwd
+
+
+# In[10]:
+
+
+get_ipython().run_cell_magic('timeit', '', '\n1+1\n2+2')
+
+
+# In[ ]:
+
+
+
+
